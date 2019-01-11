69ca551a9df7d1f7011a22a2fee81d83be716035
morticella <info@morticella.com>
2019-01-11 13:23:03 +0100

    add token in signup area

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -23,10 +23,18 @@ router.post("/api/signup", function (req, res) {
     user
       .save()
       .then(result => {
+        const token = jwt.sign(
+          { user: user.user, userId: user.hash },
+          "all_heilòljdfavonioeuòvbònx,oog.C°Slgcèpxohi455poivd5454oiu98798789hnvto098pdnuyiodutbn@#^§oiutyidoudvbdxviycv",
+          { expiresIn: "4h" }
+        );
         res.status(200).json({
+          token: token,
+          expiresIn: 3600*4,
           message: "User created!",
           result: result
         });
+
       })
       .catch(err => {
         res.status(500).json({
@@ -44,7 +52,6 @@ router.post("/api/login", (req, res, next) => {
 
   User.findOne({ user: req.body.user })
     .then(userCheck => {
-      console.log('control',userCheck);
       if (!userCheck) {
 
         return res.status(401).json({
