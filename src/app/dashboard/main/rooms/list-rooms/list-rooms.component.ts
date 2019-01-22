6053797f7b1b8e177ace1eb036d5623da0809da2
morticella <info@morticella.com>
2019-01-22 11:31:54 +0100

    New optimization of the state management and improved check allowed actions control for rooms operations to prevent operator errors

--- src/app/dashboard/main/rooms/list-rooms/list-rooms.component.ts
+++ src/app/dashboard/main/rooms/list-rooms/list-rooms.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable} from 'rxjs';
 
-import * as fromRooms from '../store/rooms.reducers';
 import * as roomsAction from '../store/rooms.actions';
 import * as buildingsAction from '../../buildings/store/building-list.actions';
 import * as customersAction from '../../customers/store/customers.actions';
@@ -14,76 +13,71 @@ import { AppState } from 'src/app/reducers';
   templateUrl: './list-rooms.component.html',
   styleUrls: ['./list-rooms.component.css']
 })
-export class ListRoomsComponent implements OnInit {
-  idBuilding: string;
+export class ListRoomsComponent implements OnInit, OnDestroy {
 
   constructor(
     private store: Store<AppState>,
     private route: ActivatedRoute
     ) {}
 
+  allowedActionControl$: any;
   roomsState$: Observable<AppState>;
   roomsState: Object;
   customersStateJSON: Object;
   customersStateString: string;
-  addNewCustomerIsPossible: boolean;
-  addNewCustomerIsPossibleEmpty: boolean;
-  rooms: any;
-  check: number;
-  checkIdRoom: string;
-  checkRefresh: string | number ;
-  stopRefresh = 0;
+  idBuilding: string;
   nameBuilding: string;
+  reloadRooms: number | string;
   reloadBuildings: number  | string;
-  customers: number | string;
-  results: number[] = [];
+  reloadCustomers: number | string;
 
   ngOnInit() {
     this.idBuilding = this.route.snapshot.params['idBuilding'];
 
     this.roomsState$ = this.store.select<AppState>('rooms');
     this.roomsState = this.roomsState$;
-    this.store.select(appState => appState).subscribe(
+    this.allowedActionControl$ = this.store.select(appState => appState).subscribe(
         appState => {
-         this.check = appState.rooms.ids.length;
-         this.customers = appState.customers.ids[0];
-         if (this.customers) {
+         this.reloadCustomers = appState.customers.ids[0];
+         this.reloadBuildings = appState.buildings.ids[0];
+         this.reloadRooms = appState.buildings.ids[0];
+         if (this.reloadCustomers ) {
           this.customersStateJSON = appState.customers.entities;
           this.customersStateString = JSON.stringify(this.customersStateJSON);
 
-         if (appState.buildings.entities[this.idBuilding]) {
-          this.nameBuilding = appState.buildings.entities[this.idBuilding].nameBuilding;
-         }
-          if (this.check === 0 && this.stopRefresh === 0) {
-            this.stopRefresh = 1;
-            this.reloadBuildings = appState.buildings.ids[0];
-            this.store.dispatch(new roomsAction.LoadingRooms());
-           }
         }
+        // if the users select view page from a building with this statment I will show the Rooms Building Name
+        if (appState.buildings.entities[this.idBuilding]) {
+          this.nameBuilding = appState.buildings.entities[this.idBuilding].nameBuilding;
+
+        }
+
       });
 
+        if (!this.reloadRooms) {
+          this.store.dispatch(new roomsAction.LoadingRooms());
+        }
         if (!this.reloadBuildings) {
           this.store.dispatch(new buildingsAction.LoadingBuildings());
         }
 
-        if (!this.customers) {
+        if (!this.reloadCustomers) {
           this.store.dispatch(new customersAction.LoadingCustomers());
         }
+
   }
 
   viewCustomerControl (idRoom: string) {
     return this.customersStateString.includes(idRoom);
   }
   newCustomerControl (idRoom: string) {
-
-  if (!this.customersStateString.includes(idRoom)) {
-    return true;
-   }
-   const regex = new RegExp(idRoom, 'gi');
-   return (this.customersStateString.match(regex) || []).length;
-
+    const regex = new RegExp(idRoom, 'gi');
+    return (this.customersStateString.match(regex) || []).length;
   }
   onDelete(id: string) {
     this.store.dispatch(new roomsAction.DeleteRoom(id));
+  }
+  ngOnDestroy() {
+    this.allowedActionControl$.unsubscribe();
   }
 }
