0aa3d45b1dfc51f89ae3bacc72f525e6bd92bada
morticella <info@morticella.com>
2019-01-27 10:52:58 +0100

    added stateService + added control allowed actions + back to ESNEXT in tsconfing becaue I decided to avoid tables + maybe Crossfilter library could improve research speed

--- src/app/dashboard/main/customers/list-customers/list-customers.component.ts
+++ src/app/dashboard/main/customers/list-customers/list-customers.component.ts
@@ -1,62 +1,61 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable} from 'rxjs';
 
-import * as customersAction from '../store/customers.actions';
-import * as roomsAction from '../../rooms/store/rooms.actions';
-import * as buildingsAction from '../../buildings/store/building-list.actions';
 import { AppState } from 'src/app/reducers';
-import { CustomersState } from '../store/customers.reducers';
+import * as customersAction from '../store/customers.actions';
+import { StateService } from '../../../../service/state.service';
 
 @Component({
   selector: 'app-list-customers',
   templateUrl: './list-customers.component.html',
   styleUrls: ['./list-customers.component.css']
 })
+
 export class ListCustomersComponent implements OnInit, OnDestroy {
 
   constructor(
     private store: Store<AppState>,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private stateService: StateService
     ) {}
 
   idRoom: string;
-  customersState$: Observable<AppState>;
-  allowedActionControl$: Observable<AppState> | any;
-  customersState: Object;
-  reloadRooms: number | string;
-  reloadBuildings: number  | string;
-  reloadCustomers: number | string;
+  customersState$: any;
+  allowedActionControl$: any;
+  reportsArray = [];
 
   ngOnInit() {
 
     this.idRoom = this.route.snapshot.params['idRoom'];
-    this.customersState$ = this.store.select<AppState>('customers');
-    this.customersState = this.customersState$;
-    this.allowedActionControl$ = this.store.select(state => state).subscribe(
-        state => {
-          this.reloadCustomers = state.customers.ids[0];
-          this.reloadRooms = state.rooms.ids[0];
-          this.reloadBuildings = state.buildings.ids[0];
-        });
-    if (!this.reloadCustomers) {
-          this.store.dispatch(new customersAction.LoadingCustomers());
-    }
-    if (!this.reloadRooms) {
-          this.store.dispatch(new roomsAction.LoadingRooms());
-    }
-    if (!this.reloadBuildings) {
-          this.store.dispatch(new buildingsAction.LoadingBuildings());
-    }
-  }
+    this.customersState$ = this.stateService.customersState$;
+    this.allowedActionControl$ = this.stateService.allowedActionControl$.subscribe(
+        appState => {
+            if (appState.reports.ids[0] && this.reportsArray.length < appState.reports.ids.length) {
+              const reports = Object.values(appState.reports.entities);
+              for (const report of reports) {
+                this.reportsArray.push(report);
+              }
 
+           }
+        });
+
+    this.stateService.reloadControl();
+
+  }
   onDelete(id: string) {
     this.store.dispatch(new customersAction.DeleteCustomer(id));
   }
 
   ngOnDestroy() {
     this.allowedActionControl$.unsubscribe();
+  }
+  // maybe I could use Crossfilter library to improve the research speed
+  checkInControl(id: string, reports) {
+    return reports.find(report => report.idCustomer === id && report.type === 'checkin');
+  }
+  checkOutControl(id: string, reports) {
+    return reports.find(report => report.idCustomer === id && report.type === 'checkout');
   }
 }
 
